1c5c1a5bac1adfb672568bc35fef98c94cfcf869
Bhati-Akash <akashbhati67@gmail.com>
2024-11-29 02:05:04 +0530

    bmi ui update

--- projects/src/bmiCalculator/bmi.js
+++ projects/src/bmiCalculator/bmi.js
@@ -1,27 +1,53 @@
 export function bmiCalculator() {
     document.getElementById('mainContent').innerHTML = `
     <div class="bmiWrapper">
-       <h1>BMI Calculator</h1>
-        <div class="bmiSections">
-            <label for="yourHeight">Height</label>
-            <span id="heightValue">174</span>
+        <div class="bmiBody">
+            <h1 id="bmiHeader">BMI Calculator</h1>
+            <div class="calculator">
+                <div class="bmiSections">
+                    <label for="yourHeight">Height</label>
+                    <span id="heightValue">174</span>
+                </div>
+                <input type="range" class="bmiRanges" name="height" id="yourHeight" min="142" max="210">
+                <div class="bmiSections">
+                    <label for="yourWeight">Weight</label>
+                    <span id="weightValue">64</span>
+                </div>
+                <input type="range" class="bmiRanges" name="weight" id="yourWeight" min="42" max="132">
+                <button id="bmiCalculator">CALCULATE</button>
+            </div>
+            <section class="output">
+                <h3 id="bmiResult">Your BMI is</h3>
+                <span id="bmiDisplay">0</span>
+                <div id="health">N/A</div>
+            </section>
+            <section class="bmiScale">
+                <div style="--color: var(--underweight)">
+                    <h4>Underweight</h4>
+                    <p>&lt; 18.5</p>
+                </div>
+                <div style="--color: var(--normal)">
+                    <h4>Normal</h4>
+                    <p>18.5 - 25</p>
+                </div>
+                <div style="--color: var(--overweight)">
+                    <h4>Overweight</h4>
+                    <p>25 - 30</p>
+                </div>
+                <div style="--color: var(--obese)">
+                    <h4>Obese</h4>
+                    <p>&ge; 30</p>
+                </div>
+            </section>
         </div>
-        <input type="range" class="bmiRanges" name="height" id="yourHeight" min="142" max="210">
-        <div class="bmiSections">
-            <label for="yourWeight">Weight</label>
-            <span id="weightValue">64</span>
-        </div>
-        <input type="range" class="bmiRanges" name="weight" id="yourWeight" min="42" max="132">
-        <button id="calculator">Calculate</button>
-        <div id="result">Your BMI is:</div>
-        <div id="health"></div>
-    <div>
+    </div>
     `
-    let btn = document.getElementById('calculator')
-    let result = document.getElementById('result')
+    let btn = document.getElementById('bmiCalculator')
+    let result = document.getElementById('bmiResult')
     let range = document.getElementById('health')
     let height = parseInt(document.getElementById('heightValue').textContent)
     let weight = parseInt(document.getElementById('weightValue').textContent)
+    const bmiDisplay = document.getElementById('bmiDisplay')
 
     function bmiCalculator(h, w) {
         h = h / 100
@@ -49,34 +75,20 @@ export function bmiCalculator() {
     })
 
     btn.addEventListener("click", () => {
-
         let bmi = bmiCalculator(...rangePicker())
-
         function bmiRangeFinder() {
-            result.textContent = `Your BMI is: ${bmi}`
-            if (bmi < 16) {
-                range.textContent = 'Severe Thinness'
-            }
-            else if (bmi < 17) {
-                range.textContent = 'Moderate Thinness'
-            }
-            else if (bmi < 18.5) {
-                range.textContent = 'Mild Thinness'
+            bmiDisplay.textContent = `${bmi}`
+            if (bmi < 18.5) {
+                range.innerHTML = `<strong>underweight</strong>`
             }
             else if (bmi < 25) {
-                range.textContent = 'Normal'
+                range.innerHTML = `<strong>normal</strong>`
             }
             else if (bmi < 30) {
-                range.textContent = 'Overweight'
+                range.innerHTML = `<strong>overweight</strong>`
             }
-            else if (bmi < 35) {
-                range.textContent = 'Obese Class 1'
-            }
-            else if (bmi < 40) {
-                range.textContent = 'Obese Class 2'
-            }
-            else if (bmi > 40) {
-                range.textContent = 'Obese Class 3'
+            else {
+                range.innerHTML = `<strong>obese</strong>`
             }
             return
         }
